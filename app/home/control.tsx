08922f1d6db394d051725c11ef497703b3269926
AlejandraZea <alejandra.d.zea@gmail.com>
2024-07-30 17:35:39 -0600

    Cambio en index, control y profile.

--- app/home/control.tsx
+++ app/home/control.tsx
@@ -4,18 +4,7 @@ import { FontAwesome } from '@expo/vector-icons';
 import { movement } from '../../scripts/moveCar';
 
 export default function ControlScreen() {
-  // // Datos de ejemplo para las ondas cerebrales
-  // const brainWaveData = {
-  //   labels: ["1s", "2s", "3s", "4s", "5s", "6s"],
-  //   datasets: [
-  //     {
-  //       data: [20, 45, 28, 80, 99, 43],
-  //       color: (opacity = 1) => `rgba(134, 65, 244, ${opacity})`, // color de la línea
-  //       strokeWidth: 2, // grosor de la línea
-  //     }
-  //   ],
-  //   legend: ["Ondas cerebrales"]
-  // };
+  
   const [isRunning, setIsRunning] = useState(false);
 
   const handlePress = () => {
@@ -27,7 +16,6 @@ export default function ControlScreen() {
   return (
     <View style={styles.container}>
       <Text style={styles.header}>Panel de Control</Text>
-      <Text style={styles.text}> Controla el dispositivo a traves de los siguientes botones</Text>
       <View style={styles.buttonRowUpDown}>
         <FontAwesome.Button
           name="arrow-up"
@@ -36,7 +24,6 @@ export default function ControlScreen() {
           size={40}
           onLongPress={() => movement(true,"UP")}
           onPressOut={() => movement(false,"UP")}
-          // onPress={() => console.log('Move Up')}
           style={styles.button}
         >
           
@@ -59,7 +46,7 @@ export default function ControlScreen() {
         <FontAwesome.Button
          name={isRunning ? "play" : "stop"}
          backgroundColor={isRunning ? "#5cb85c" : "#d9534f"}
-         size={20}
+         size={20}          
          onPress={handlePress}
          style={styles.button}
        >
@@ -87,94 +74,59 @@ export default function ControlScreen() {
           size={40}
           onLongPress={() => movement(true,"DOWN")}
           onPressOut={() => movement(false,"DOWN")}
-          // onPress={() => console.log('Move Down')}
           style={styles.button}
         >
           
         </FontAwesome.Button>
       </View>
-
-      {/* <Text style={styles.graphHeader}>Ondas Cerebrales</Text>
-      <View style={styles.graphContainer}>
-        <LineChart
-          data={brainWaveData}
-          width={Dimensions.get('window').width * 0.8} // 80% del ancho de la pantalla
-          height={200} // Altura del gráfico ajustada
-          chartConfig={chartConfig}
-          bezier
-          style={styles.graph}
-        />
-      </View> */}
     </View>
   );
 }
 
-// const chartConfig = {
-//   backgroundGradientFrom: "#ffffff",
-//   backgroundGradientTo: "#ffffff",
-//   color: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
-//   labelColor: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
-//   style: {
-//     borderRadius: 16,
-//   },
-//   propsForDots: {
-//     r: "6",
-//     strokeWidth: "2",
-//     stroke: "#ffa726"
-//   }
-// };
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#FFFFFF',
     justifyContent: 'center',
     alignItems: 'center',
-    padding: 16,
+    padding: 10,
   },
   header: {
-    fontSize: 24,
+    fontSize: 34,
     textAlign: 'center',
     marginVertical: 20,
+    color: '#042654',
+    fontWeight: 'bold',
   },
   buttonRowUpDown: {
-    flexDirection: 'row', 
+    flexDirection: 'row',
     paddingHorizontal: 10,
     marginHorizontal: 15,
-    marginBottom: 10, 
+    marginBottom: 10,
   },
-  buttonRow: {
+  buttonRow: {    
     flexDirection: 'row',
     marginVertical: 15,
     marginHorizontal: 15,
-    marginBottom: 15,    
-    justifyContent: 'space-between',
-    width: '100%',
+    marginBottom: 15,   
+    justifyContent: 'space-around',
+    paddingLeft: 5,
+    paddingRight: 5,
+    backgroundColor: '#F8F9FA',
+    borderRadius: 10,
+    
+
   },
   button: {
-    paddingHorizontal: 10,
-    marginHorizontal: 15,
-    width: 75,
+    paddingHorizontal: 45,
+    marginHorizontal: 10,
+    width: 85,
     height: 95,
-    shadowColor: '#fff',
-    shadowOpacity: 0.9,
     justifyContent: 'center',
+    alignItems: 'center',
+    shadowColor: '#000',
+    padding: 10,
+    marginRight: -5,
+    
   },
-  text: {
-    fontSize: 16,
-    textAlign: 'center',
-    marginBottom: 16,
-  },
-  // graphHeader: {
-  //   fontSize: 20,
-  //   textAlign: 'center',
-  //   marginVertical: 20,
-  // },
-  // graphContainer: {
-  //   alignItems: 'center', // Centrar el gráfico horizontalmente
-  // },
-  // graph: {
-  //   marginVertical: 8,
-  //   borderRadius: 16,
-  // },
 });
